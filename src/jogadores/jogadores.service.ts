11bad4f007fe1b8d91feba102a176b77809e2bbd
WagnerReis <wagnerreis1997@gmail.com>
2022-10-05 15:21:27 -0300

    criando service de jogadores

---
+++ src/jogadores/jogadores.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-Jogador.dto';
+import { Jogador } from './interfaces/jogador.interface';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class JogadoresService {
+  private jogadores: Jogador[] = [];
+  private readonly logger = new Logger(JogadoresService.name);
+
+  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
+    this.criar(criarJogadorDto);
+  }
+
+  private criar(criaJogadorDto: CriarJogadorDto): void {
+    const { nome, email, telefoneCelular } = criaJogadorDto;
+
+    const jogador: Jogador = {
+      _id: uuid(),
+      nome,
+      email,
+      telefoneCelular,
+      ranking: `A`,
+      posicaoRanking: 1,
+      urlFotoJogador: 'www.google.com.br/foto123.jpg',
+    };
+    this.logger.log(`criaJogadorDto: ${JSON.stringify(jogador)}`);
+    this.jogadores.push(jogador);
+  }
+}
